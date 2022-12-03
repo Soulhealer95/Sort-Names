f47b486f18d5cf8073cb2058cc13b74adbc8af0c
Shivam Saxena <sstshivam1@hotmail.com>
2022-12-03 08:22:56 -0500

    working 1

--- sort_names.c
+++ sort_names.c
@@ -8,7 +8,7 @@
 #define SPACE	' '
 
 typedef struct node {
-	struct node* valid[ALPHABETS+1];
+	struct node* valid[ALPHABETS+2];
 	int end_of_line;
 	int root_count; // This is clever way to support parallelization.
 } node;
@@ -21,13 +21,11 @@ node* create_alphabets(node* output, int* root_letter, int end) {
 			printf("Not enough Memory!\n");
 			return NULL;
 		}
-//		for (int i =0; i < ALPHABETS; i++) {
-//			output->valid[i] = NULL;
-//		}
 		(output)->end_of_line = end;
 	}
 	// Node should already exist, set a letter but dont change anything if end_of_line was set
-	if ((output)->end_of_line != end && end == 1) {
+	if (output->end_of_line != 1 && end == 1) {
+		printf("setting eOl\n");
 		(output)->end_of_line = 1;
 	}
 	// Either way, we've added to the root so let's update that
@@ -36,12 +34,11 @@ node* create_alphabets(node* output, int* root_letter, int end) {
 }
 
 void edit_node(node* place, int letter,int* root_letter, int end) {
-	if (letter < 0 || letter >= ALPHABETS || root_letter == NULL) {
+	if (letter < 0 || letter >= ALPHABETS+1 || root_letter == NULL) {
 		printf("Couldn't edit node\n");
 		return;
 	}
 	// Check if node already exists. set same status
-	printf("About to create a new node\n");
 	place->valid[letter] = create_alphabets(place->valid[letter],root_letter, end);
 	return;
 }
@@ -55,11 +52,14 @@ void print(node* root) {
 		if(root->valid[i] == NULL) {
 			continue;
 		}
-		if (i == 27) {
+		if (i == ALPHABETS) {
 			printf(" ");
 		}
 		else {
 			printf("%c", i + ALPHABET);
+		}
+		if (root->valid[i]->end_of_line == 1) {
+			printf("\n");
 		}
 		// Go further into this
 		print(root->valid[i]);
@@ -96,13 +96,10 @@ void place_in_tree(char* input, node* root) {
 			printf("Invalid characters detected. skipping - '%c'\n", lower);
 			continue;
 		}
-		if(!(*input+1)) {
+		if(*(input+1) == '\0') {
 			end = 1;
 		}
-		// This could be done in parallel
-		printf("Trying to create node for %c - %d\n", lower, index);
-		edit_node(ptr, index ,&root->root_count, end);
-		printf("Finished editing!\n");
+		edit_node(ptr, index, &root->root_count, end);
 		ptr = ptr->valid[index];
 	}
 	return;
@@ -117,12 +114,15 @@ void place_in_tree(char* input, node* root) {
 
 int main(void) {
 	// Get input
-	char* input = "Marie Eve Denis";
+	char* input = "M";
 	node* root = malloc(sizeof(node));
 	root->root_count = 0;
 	root->end_of_line = 0;
 	place_in_tree(input, root);
+	char* second = "Shiv S";
+	place_in_tree(second, root);
 	// Place in data structure
 	print(root);
+	printf("\n");
 	return 0;
 }
