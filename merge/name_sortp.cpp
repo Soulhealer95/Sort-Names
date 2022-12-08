51e7fabdc1362b6a6cea70e7192ac2d2ecc3b60e
shisaxena <shisaxena@blackberry.com>
2022-12-08 14:33:11 -0500

    working commit

--- merge/name_sortp.cpp
+++ merge/name_sortp.cpp
@@ -1,80 +1,112 @@
 #include <iostream>
 #include <cstring>
 #include <fstream>
+#include <atomic>
+#include <chrono>
+#include <thread>
 
 // Definitions
 #define NAME_SIZE 25
 #define FILE_SIZE		140190
 #define FILE_NAME		"../names.txt"
+#define MAX_DEPTH	10
+#define ITEMS	10000
+#define TEST_ITEMS	ITEMS
+
+pthread_mutex_t big_lock = PTHREAD_MUTEX_INITIALIZER;
 
 typedef struct data {
 	char** list;
 	int start;
 	int end;
+	int depth;
+//	std::atomic_int* depth;
 } data;
 
 // Prototypes
 void swap(char** list, char** temp, int start, int end);
 int compare(char* x, char* y);
 void merge(char** list, int start, int mid, int end);
-void simple_merge(data* info);
+void* simple_merge(char** list, int start, int end, std::atomic_int* depth);
 void create_list(char** start, int len, int size);
 char** get_namesf(const char* file_name , int file_size, int items);
 
 // Actual Code
 int main() {
-	int len = 10000;
+	int len = ITEMS;
 	char** list = get_namesf(FILE_NAME , FILE_SIZE, len);
 	int size = NAME_SIZE;
+	std::atomic_int* depth;
+	depth = new std::atomic_int [1];
+	*depth = 0;
 	data* tinfo;
 	tinfo = new data [1];
 	tinfo->list = list;
 	tinfo->start = 0;
-	tinfo->end = len-1;
-	simple_merge(tinfo);
-	for(int i =0; i < len; i++) {
+	tinfo->end = TEST_ITEMS-1;
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+	simple_merge(tinfo->list, 0, tinfo->end, depth);
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+	std::cout << "Sort time (sec) = " << (std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count())/1000000.0 << std::endl;
+
+	//print_list -- debugging only
+	for(int i =0; i < TEST_ITEMS; i++) {
 		std::cout << list[i] << "\n";
 	}
+	// end print
 	delete [] list;
 	return 0;
 }
 
 // Function Implementations
-void simple_merge(data* info) {
-	if(!info) {
-		return;
-	}
+void* simple_merge(char** list, int start, int end, std::atomic_int* depth) {
 	// one or less element
 	// already sorted! return
-	if(!info->list || info->start >= info->end) {
-		return;
+	if(!list || start >= end) {
+		return NULL;
 	}
 	int mid = 0;
-	int r_start = info->start;
-	int r_end = info->end;
-	int  len = r_end - info->start + 1;
+	int r_start = start;
+	int r_end = end;
+	int  len = r_end - start + 1;
+
 	// find mid
 	if(len == 2) {
-		mid = info->start;
+		mid = start;
 	}
 	else if(len % 2 !=0) {
-		mid =  info->start + (len-1)/2;
+		mid = start + (len-1)/2;
 	}
 	else {
-		mid = info->start + len/2;
+		mid = start + len/2;
 	}
 
-	// sort left half
-	info->end = mid;
-	simple_merge(info);
-	//sort right half
-	info->start = mid+1;
-	info->end = r_end;
-	simple_merge(info);
+	if(std::atomic_load(depth) >= MAX_DEPTH) {
+		// sort left half
+		simple_merge(list, start, mid, depth);
+		//sort right half
+		simple_merge(list, mid+1, end, depth);
+	}
+	else {
+		std::atomic_store(depth,(std::atomic_load(depth) + 1));
+		// start threads
+		// sort left
+		std::thread left(simple_merge, list, start, mid, depth);
+
+		// sort right half
+		std::thread right(simple_merge, list, mid+1, end, depth);
+
+		// Wait for halves to be sorted
+		left.join();
+		right.join();
+	}
 
 	// merge halves
-	merge(info->list, r_start, mid, r_end);
-	return;
+	pthread_mutex_lock(&big_lock);
+	merge(list, r_start, mid, r_end);
+	pthread_mutex_unlock(&big_lock);
+
+	return NULL;
 }
 
 
