88c588a5ea3cf386a2f7895ed62ab4b17d3999b7
Shivam Saxena <sstshivam1@hotmail.com>
2022-12-15 02:45:24 -0500

    Updated Parallel implementation. Best run 1.29ms.

--- merge/p_merge.cpp
+++ merge/p_merge.cpp
@@ -1,24 +1,45 @@
 /*					Parallel Merge Sort
  * Sort a list of names by last name then first name
  * Algorithm: Merge Sort
- * Fastest Speed: ~3ms @ depth = 10
+ * Fastest Speed: ~1.29ms @ depth = 2 [note, this speed varies!]
+ *
+ * Compilation Instructions:
+ * $ g++ -g -O2 p_merge.cpp -o psort -lpthread && ./psort
+ * Sort time (ms) = 1.898
+ *
+ * Tested on x86_64 Ubuntu 18
  *
  * Description:
- * Pretty straight forward textbook implementation that works.
- * Doesn't print the list but can be made to.
- * Only thing it does better is when it gets data from disk, it organizes it
- * as lastname_firstname. so minor tweaks needed if they want to see it.
- * It's no different than using a struct to store it differently.
+ * Pretty straight forward textbook implementation of merge sort that works.
  *
- * Author:													Dated
- * Shivam S.												08-Dec-22
+ * Here's the twist:
+ * Thing it does better is when it gets data from disk, it organizes it
+ * as lastname_firstname into a make shift hash table.
+ * That's exactly the same as storing it in any other struct.
+ * People optimize by splitting strings early into struct members, 
+ * I decided to keep them together but place them into a table instead.
+ *
+ * Basically each of this table entries is indexed by alphabets
+ * So last name with same starting letters are together. This is the simplest
+ * and fastest hash function as it's pretty much constant time.
+ *
+ * Then 26 threads are created to solve roughly n/26th problem "at once"
+ * each thread sorting names starting with same last name initial letter
+ * and time is recorded for them to finish. 
+ * 
+ * Key thing is that DEPTH of each thread is simply a parallel gimmick
+ * Real parallelization is the splitting of work early on
+ *
+ * Doesn't print the list but can be made to with DEBUG option
+ *
+ * Author:												Dated
+ * Shivam S.											15-Dec-22
  * Student, McMasterU
  *
  */
 #include <iostream>
 #include <cstring>
 #include <fstream>
-#include <atomic>
 #include <chrono>
 #include <thread>
 
@@ -26,47 +47,70 @@
 #define NAME_SIZE 25
 #define FILE_SIZE		140190
 #define FILE_NAME		"../names.txt"
-#define MAX_DEPTH	10
+#define MAX_DEPTH	2
 #define ITEMS	10000
+#define ALPHABET_START	'A'
+#define ALPHABETS	26
 // This define is used mostly for debugging as it can limit items to sort
 #define TEST_ITEMS	ITEMS
+#define DEBUG 0
+
+// let's create a hash table 
+typedef struct hash_t {
+		char* NAMES[ITEMS];
+		int count;
+} hash_t;
+
+
 
 // Prototypes
 void swap(char** list, char** temp, int start, int end);
 int compare(char* x, char* y);
 void merge(char** list, int start, int mid, int end);
-void* simple_merge(char** list, int start, int end, std::atomic_int* depth);
+void* simple_merge(char** list, int start, int end, int depth);
 void create_list(char** start, int len, int size);
-char** get_namesf(const char* file_name , int file_size, int items);
+
+// Hash table functions
+int get_namesf(const char* file_name , int items, hash_t* table);
+void place_in_table(const char* name, int size, hash_t* table);
+void print_table(hash_t* table, int size);
+void del_table(hash_t* table, int size);
 
 // Actual Code
 int main() {
 	// Get some vars in the picture
-	int range_end, len = ITEMS;
-	std::atomic_int* depth;
-	// get list of names from the file
-	char** list;
-	list = get_namesf(FILE_NAME , FILE_SIZE, len);
-	depth = new std::atomic_int [2]();
-	range_end = TEST_ITEMS-1;
+	hash_t* table;
+	table = new hash_t[ALPHABETS]();
+	if(get_namesf(FILE_NAME, ITEMS, table) == -1) {
+		return -1;
+	}
+	std::thread t[ALPHABETS];
 
 	// Start Sorting!
-	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-	simple_merge(list, 0, range_end, depth);
-	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-	std::cout << "Sort time (sec) = " << (std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count())/1000000.0 << std::endl;
-
-	/* print_list -- debugging only *
-	for(int i =0; i < TEST_ITEMS; i++) {
-		std::cout << list[i] << "\n";
+	for(int i =0; i < ALPHABETS; i++) {
+		t[i] = std::thread (simple_merge, table[i].NAMES, 0, table[i].count-1,0);
 	}
-	//end print*/
-	delete [] list;
+	// start timing once all threads are working
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+	// sorting is done when all threads are done
+	for(int i =0; i < ALPHABETS; i++) {
+		t[i].join();
+	}
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+	// print results
+	std::cout << "Sort time (ms) = " << (std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count())/1000.0 << std::endl;
+
+	if(DEBUG) {
+		print_table(table,ALPHABETS);
+	}
+
+	// cleanup
+	del_table(table, ALPHABETS);
 	return 0;
 }
 
 // Function Implementations
-void* simple_merge(char** list, int start, int end, std::atomic_int* depth) {
+void* simple_merge(char** list, int start, int end, int depth) {
 	// one or less element
 	// already sorted! return
 	if(!list || start >= end) {
@@ -88,14 +132,14 @@ void* simple_merge(char** list, int start, int end, std::atomic_int* depth) {
 		mid = start + len/2;
 	}
 
-	if(std::atomic_load(depth) >= MAX_DEPTH) {
+	if(depth >= MAX_DEPTH) {
 		// sort left half
 		simple_merge(list, start, mid, depth);
 		//sort right half
 		simple_merge(list, mid+1, end, depth);
 	}
 	else {
-		std::atomic_store(depth,(std::atomic_load(depth) + 1));
+		depth++;
 		// start threads
 		// sort left
 		std::thread left(simple_merge, list, start, mid, depth);
@@ -187,32 +231,63 @@ void merge(char** list, int start, int mid, int end) {
 
 	return;
 }
-// Just some IO function. Please Free after :)
-char** get_namesf(const char* file_name , int file_size, int items) {
-	using namespace std;
-	//allocate mem to return buffer
-	char** return_str;
-	return_str = new char* [items];
-	create_list(return_str, items, NAME_SIZE);
-	char tbuffer [NAME_SIZE];
-	char fname[NAME_SIZE];
-	char lname[NAME_SIZE];
-	int word = 0;
-	// read file, store  to list as lastname+firstname
-	ifstream file(FILE_NAME);
-	while(file >> fname >> lname) {
-			strcat(return_str[word],lname);
-			strcat(return_str[word]," ");
-			strcat(return_str[word],fname);
-			word++;
-	}
-	/* debugging purposes only
-	for(int p =0; p < items;p++){
-		cout << return_str[p] << endl;
-	}
-	*/
-	return return_str;
 
-	file.close();
+// helper functions for hash table
+int to_index(char character) {
+		return (int) (character - ALPHABET_START);
 }
 
+// gets a first/last name and places into a hash table 
+void place_in_table(const char* fname, const char* lname, int size, hash_t* table) {
+		int ptr = 0;
+		hash_t* t_ptr = &table[to_index(lname[0])];
+		// place the name
+		char* tbuffer;
+		tbuffer = new char[size]();
+		memset(tbuffer, '\0', size);
+		strcpy(tbuffer, lname);
+		strcat(tbuffer, " ");
+		strcat(tbuffer,fname);	
+		t_ptr->NAMES[t_ptr->count]= tbuffer;
+		t_ptr->count++;
+
+		return;
+}
+
+// Get names from file IO and print
+int get_namesf(const char* file_name , int items, hash_t* table) {
+	// read file, store  to list as lastname+firstname
+	std::ifstream file (FILE_NAME);
+	if(!file) {
+			std::cout << "Error Opening File";
+			return -1;
+	}
+	char fname[NAME_SIZE];
+	char lname[NAME_SIZE];
+	while(file >> fname >> lname) {
+		place_in_table(fname, lname, NAME_SIZE, table);
+	}
+	file.close();
+	return 0;
+
+}
+
+// iterate over names within each table entry and print
+void print_table(hash_t* table, int size) {
+		for(int i = 0; i < size; i++) {
+				for(int j = 0; j < table[i].count; j++) {
+						printf("%s\n", table[i].NAMES[j]);
+				}
+		}
+
+}
+// clean up allocated memory
+void del_table(hash_t* table, int size) {
+		for(int i = 0; i < size; i++) {
+				// delete all allocated names
+				for(int j = 0; j < table[i].count; j++) {
+						delete []table[i].NAMES[j];
+				}
+		}
+		delete [] table;
+}
